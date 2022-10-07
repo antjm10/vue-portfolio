906c5a6de3fe745fb69223f6c9cfa5f59485bf9d
Anthony <anthony.mieville@jobtrek.ch>
2022-10-07 09:20:13 +0200

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/views/home-about.vue';
 import Project from '@/views/project-work.vue';
 import Contact from '@/views/contact-me.vue';
 
+// tableau des routes
 const routes = [
     {
         path: "/",
